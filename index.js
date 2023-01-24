d223c8a675d2cd15dd2fde53937b4bfe8031da5c
berger <robertberger.dev@gmail.com>
2023-01-24 14:03:57 -0500

    ran npm start

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 "use strict";
-class Greeting {
-    constructor(greeterName) {
-        this.name = greeterName;
-    }
-    greet() {
-        return `Hello ${this.name}!`;
+// class Greeting {
+//     name: string;
+//     constructor(greeterName: string) {
+//         this.name = greeterName;
+//     }
+//     greet(): string {
+//         return `Hello ${this.name}!`;
+//     }
+// }
+// let newGreeting = new Greeting("James");
+// console.log(newGreeting.greet());
+class Animal {
+    constructor(animalName) {
+        this.name = animalName;
     }
 }
-let newGreeting = new Greeting("James");
-console.log(newGreeting.greet());
+class Cow extends Animal {
+    constructor(name) {
+        super(name);
+    }
+    move(distance = 2) {
+        console.log(`${this.name} moved ${distance} units.`);
+    }
+}
+let bob = new Cow("Bob");
+bob.move();
