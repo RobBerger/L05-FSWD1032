d4205f8a01d7a6d1b86ffc4d51d35948cee77eb6
berger <robertberger.dev@gmail.com>
2023-01-24 17:14:28 -0500

    added a getter and setter

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 class Person {
-    name: string = "Nelson";
+    private _name: string = "Nelson";
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(newName: string) {
+        if (newName != "") {
+            this._name = newName;
+        }
+    }
 }
 
-let Bob = new Person();
+let Bob = new Person ();
 Bob.name = "Bob Bertson";
 console.log(Bob.name);
