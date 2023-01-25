bf14361ebbed8dd1225ff4e38cb725919932644d
berger <robertberger.dev@gmail.com>
2023-01-25 15:58:38 -0500

    ran npm start

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 "use strict";
-// class Person {
-//     private _name: string = "Nelson";
-//     get name(): string {
-//         return this._name;
-//     }
-//     set name(newName: string) {
-//         if (newName != "") {
-//             this._name = newName;
-//         }
-//     }
-// }
-// let Bob = new Person ();
-// Bob.name = "Bob Bertson";
-// console.log(Bob.name);
-// class Counter {
-//     private static count = 0;
-//     static increment(){
-//         Counter.count++;
-//     }
-//     static getCount(){
-//         return Counter.count;
-//     }
-// }
-// for (let i = 0; i < 10; i++){
-//     Counter.increment();
-// }
-// console.log(`Count: ${Counter.getCount()}`);
-class Circle {
-    constructor(r) {
-        this.radius = r;
-    }
-    getArea() {
-        return Circle.PI * this.radius * this.radius;
+class Animal {
+    onTheMove() {
+        console.log('The animal is on the move!');
     }
 }
-Circle.PI = 3.14;
-let myCircle = new Circle(10);
-console.log(myCircle.getArea());
+class Cat extends Animal {
+    meow() {
+        console.log('Meow!');
+    }
+}
+let cat = new Cat();
+cat.onTheMove();
+cat.meow();
