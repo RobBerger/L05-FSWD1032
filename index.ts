51c80b78317b604b900006ebdcff3e4f2c1b6086
berger <robertberger.dev@gmail.com>
2023-01-25 15:53:09 -0500

    replaced all code in index.ts

--- index.ts
+++ index.ts
@@ -1,50 +1,15 @@
-// class Person {
-//     private _name: string = "Nelson";
-
-//     get name(): string {
-//         return this._name;
-//     }
-
-//     set name(newName: string) {
-//         if (newName != "") {
-//             this._name = newName;
-//         }
-//     }
-// }
-
-// let Bob = new Person ();
-// Bob.name = "Bob Bertson";
-// console.log(Bob.name);
-
-// class Counter {
-//     private static count = 0;
-
-//     static increment(){
-//         Counter.count++;
-//     }
-
-//     static getCount(){
-//         return Counter.count;
-//     }
-// }
-
-// for (let i = 0; i < 10; i++){
-//     Counter.increment();
-// }
-
-// console.log(`Count: ${Counter.getCount()}`);
-
-class Circle {
-    static readonly PI = 3.14;
-    private radius: number;
-
-    constructor(r: number) {
-        this.radius = r;
-    }
-
-    getArea(): number {
-        return Circle.PI * this.radius * this.radius;
+class Animal {
+    onTheMove(){
+        console.log('The animal is on the move!');
     }
 }
-let myCircle= new Circle(10);
-console.log(myCircle.getArea());
+
+class Cat extends Animal {
+    meow(){
+        console.log('Meow!')
+    }
+}
+
+let cat = new Cat();
+cat.onTheMove();
+cat.meow();
